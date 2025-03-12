6e5de0c4c5ae5607f810ebd920c044d93cdc7790
Muhammad Syahril Darmawan <123642882+SyahrilDarmawan@users.noreply.github.com>
2025-03-12 21:02:47 +0700

    Perubahan pada File models.py
    Memindahkan sintaks body html dari xml ke .py untuk membuat sebuah nama Partner dan nomor Invoice secara otomatis melalui Invoice di Accounting

--- vit_send_invoice/models/models.py
+++ vit_send_invoice/models/models.py
@@ -16,6 +16,26 @@ class vit_send_invoice(models.Model):
         return False
 
     def action_send_email(self):
+        # Buat body HTML secara manual
+        body_html = f"""
+            <div>
+                Halo <strong>{self.partner_id.name or 'Dear Partner'}</strong>,
+                <br/>
+                Invoice Anda <strong>#{self.name}</strong> telah divalidasi!
+                <br/>
+                Terima Kasih,
+                <br/>
+                Tim Admin
+            </div>
+        """
+        
+        # Ambil template dasar
         template = self.env.ref('vit_send_invoice.email_notif_post')
-        template.send_mail(self.id, force_send=True)
-    
+        
+        # Kirim email dengan body yang sudah dibuat
+        email_values = {
+            'body_html': body_html,
+            'subject': self.name,  # Pastikan subject juga sesuai
+            'email_to': self.partner_id.email,  # Pastikan email penerima
+        }
+        template.send_mail(self.id, force_send=True, email_values=email_values)
